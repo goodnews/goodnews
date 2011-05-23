bc1a07bcff37227e0f3b21e378db3dd04ad23858
Good News <tung.berlin@googlemail.com>
2011-05-23 16:38:02 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodnews::Application.config.secret_token = 'bdc9fc935388e4fd29bd315d5b3f734367cd0d7a6f39a84387651ab8d91b49959996ab8462c6776fece3b3d8d9409550f6e36da6f11b520fcdf97dc73123717c'
